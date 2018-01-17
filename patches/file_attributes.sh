32ac98f5dc001fe9ec7b5e2c7d78a25557df6851
AlistairGrant <akgrant0710@gmail.com>
2018-01-17 18:47:15 +0100

    file_attributes.sh: Don't copy FileAttributesPlugin/

--- patches/file_attributes.sh
+++ patches/file_attributes.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #
-# Patch the sources to include FileAttributes for pharo.cog.spur on:
-#   linux 32 bit, linux 64 bit
+# Patch the make files to include FileAttributes for pharo.cog.spur on:
+#   linux 32 bit, linux 64 bit, Win32, Win64
 #
 
-echo "Update FileAttributesPlugin source"
-cp -ar patches/FileAttributesPlugin pharo-vm/opensmalltalk-vm/platforms/Cross/plugins/
 grep -q FileAttributesPlugin pharo-vm/opensmalltalk-vm/build.linux32x86/pharo.cog.spur/plugins.ext
 if [ $? -eq 1 ]
 then
